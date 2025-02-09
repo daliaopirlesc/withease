ad8fd03da9d72f43ae2edc88ccf38bac13f9f160
daliaopirlesc <103562288+daliaopirlesc@users.noreply.github.com>
2025-02-09 18:25:29 +0200

    added backend initializer

---
+++ backend/src/main/java/com/with/ease/with_ease_backend/WithEaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.with.ease.with_ease_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithEaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithEaseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/with/ease/with_ease_backend/WithEaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.with.ease.with_ease_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithEaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
